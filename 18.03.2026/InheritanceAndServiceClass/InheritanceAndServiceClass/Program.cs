097c3bb9c46273a8c845abda8ebd44f1673c13c0
Opilane TTHK <opilane@tthk.ee>
2026-03-19 09:25:09 +0200

    update

--- 18.03.2026/InheritanceAndServiceClass/InheritanceAndServiceClass/Program.cs
+++ 18.03.2026/InheritanceAndServiceClass/InheritanceAndServiceClass/Program.cs
@@ -2,6 +2,7 @@
 using InheritanceAndServiceClass.AppServices.Services;
 using InheritanceAndServiceClass.Core.ServiceInterface;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,13 +10,56 @@ namespace InheritanceAndServiceClass
 {
     internal class Program
     {
+        private readonly ICarServices _carServices;
+
+        public Program
+        (
+            ICarServices carServices
+        )
+        {
+            _carServices = carServices;
+        }
+
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddScoped<ICarServices, CarServices>();
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Switch!");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+                case 1:
+                    var app = builder.Build();
+                    using (var scope = app.Services.CreateScope())
+                    {
+                        var carServices = scope.ServiceProvider.GetRequiredService<ICarServices>();
+                        var program = new Program(carServices);
+                        program.GetAsync();
+                    }
+                    break;
+
+
+                default:
+                Console.WriteLine("Error");
+                    break;
         }
     }
+
+        public IActionResult GetAsync()
+        {
+            _carServices.GetData();
+
+        return View();
+        }
+
+    private IActionResult View()
+    {
+        throw new NotImplementedException();
+    }
+    }
 }
+
+
