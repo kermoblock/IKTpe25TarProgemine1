8a3bf3d23b118269909ac6a929403d0ca8a2b6cd
Opilane TTHK <opilane@tthk.ee>
2026-03-19 09:07:33 +0200

    Mark

--- 18.03.2026/InheritanceAndServiceClass/InheritanceAndServiceClass/Program.cs
+++ 18.03.2026/InheritanceAndServiceClass/InheritanceAndServiceClass/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using InheritanceAndServiceClass.AppServices.Services;
+using InheritanceAndServiceClass.Core.ServiceInterface;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace InheritanceAndServiceClass
@@ -8,6 +12,10 @@ namespace InheritanceAndServiceClass
         static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<ICarServices, CarServices>();
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
