d05941be7f330828ac4d8f043cffe9eccb97e847
Opilane TTHK <opilane@tthk.ee>
2026-05-07 14:57:20 +0300

    Controller

---
+++ University/University/University/ViewModel/EnrollmentDateGroupViewModel.cs
@@ -0,0 +1,10 @@
+﻿using University.Controllers;
+
+namespace University.ViewModel
+{
+    public class EnrollmentDateGroupViewModel
+    {
+        public DateTime? EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
