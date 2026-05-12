0f3c31c7e7fe0bccd3c7b6e8cf47b1d6919a4f19
kermoblock <mrdisxo@gmail.com>
2026-05-12 14:05:27 +0300

    Parameetrid

--- University/University/University/ViewModel/StudentIndexViewModel.cs
+++ University/University/University/ViewModel/StudentIndexViewModel.cs
@@ -1,10 +1,23 @@
-﻿namespace University.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.ViewModel
 {
     public class StudentIndexViewModel
     {
         public int Id { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+
         public string LastName { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+        [Column("FirstName")]
+
         public string FirstMidName { get; set; }
+        [StringLength(50, MinimumLength = 1)]
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
         public DateTime EnrollmentDate { get; set; }
     }
 }
