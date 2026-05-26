5edcb6632b033b51cb2796555a5949e02c37f783
Opilane TTHK <opilane@tthk.ee>
2026-05-26 15:55:23 +0300

    CourseIndex

---
+++ University/University/University/Controllers/CourseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+
+namespace University.Controllers
+{
+    public class CourseController : Controller
+    {
+        //on vaja kutsuda välja University constructor
+
+        private readonly UniversityContext _context;
+
+        public CourseController
+            (
+                UniversityContext context
+            )
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Courses
+                .Include(c => c.Departments)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(result);
+        }
+    }
+}
---
+++ University/University/University/ViewModel/CoursesVM/CourseIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using University.Models;
+
+namespace University.ViewModel.CourseVM
+{
+    public class CourseIndexViewModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+        public int DepartmentId { get; set; }
+
+        //vaja department classile viidata ja sealt tahame omakorda Name
+        public Department Department { get; set; }
+
+    }
+}
